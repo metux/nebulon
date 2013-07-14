2e01a075d1e6e3d26287133d207c0feba617fcb5
Enrico Weigelt, metux ITS <weigelt@metux.de>
2013-07-14 23:46:28 +0200

    1

--- src/de/metux/nebulon/util/Encoder.java
+++ src/de/metux/nebulon/util/Encoder.java
@@ -1,7 +1,10 @@
 package de.metux.nebulon.util;
 
-import java.io.*;
-import java.util.zip.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 public class Encoder {
 
